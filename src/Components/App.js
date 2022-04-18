9a6f062a6894470053ee2915d67d64b36b156bc5
Ben Hessel <benhessel@Bens-MacBook-Air.local>
2022-04-17 20:44:34 -0400

    fixed useEffect error by putting function in useEffect

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,9 +10,8 @@ import axios from 'axios';
 
 function App() {
 
-  const [user, setUser] = useState({});
-  const [loggedInStatus, setLoggedInStatus] = useState('not_logged_in');
-  const [logout, setLogout] = useState('');
+  const [user, setUser] = useState({})
+  const [loggedInStatus, setLoggedInStatus] = useState('not_logged_in')
 
   const handleLogin = (data) => {
     setLoggedInStatus('logged_in')
@@ -24,27 +23,36 @@ function App() {
     setUser({})
   }
 
-  const checkLoginStatus = () => {
-    axios.get('http://localhost:3000/logged_in',
-      { withCredentials: true })
-      .then(response => {
-        console.log('response test', response)
-        if (response.data.logged_in && loggedInStatus === 'not_logged_in') {
-          setLoggedInStatus('logged_in')
-          setUser(response.data.user)
-      } else if (!response.data.logged_in && loggedInStatus === 'logged_in') {
-        setLoggedInStatus('not_logged_in')
-        setUser({})
-      }
-    })
-      .catch(error => {
-        console.log('check login error?', error)
-      })
-  }
-
+  
   useEffect(() => {
-    checkLoginStatus()
+    const checkLoginStatus = () => {
+      axios.get('http://localhost:3000/logged_in',
+        { withCredentials: true })
+        .then(response => {
+          console.log('logged in?:', response)
+          if (response.data.logged_in && loggedInStatus === 'not_logged_in') {
+            setLoggedInStatus('logged_in')
+            setUser(response.data.user)
+        } else if (!response.data.logged_in && loggedInStatus === 'logged_in') {
+          setLoggedInStatus('not_logged_in')
+          setUser({})
+        }
+      })
+        .catch(error => {
+          console.log('check login error?', error)
+        })
+    }
+    return () => {
+      checkLoginStatus()
+    }
   }, [])
+
+  // useEffect(() => {
+  //   effect
+  //   return () => {
+  //     cleanup
+  //   };
+  // }, [input]);
 
 
   return (
--- src/Components/auth/Login.js
+++ src/Components/auth/Login.js
@@ -1,67 +1,74 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = ({ handleSuccessfulAuth }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [loginErrors, setLogginErrors] = useState('')
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    // const [loginErrors, setLogginErrors] = useState('')
+  const handleEmail = (e) => {
+    // console.log(e)
+    setEmail(e.target.value);
+  };
 
-    const handleEmail = (e) => {
-        console.log(e)
-        setEmail(e.target.value)
-    }
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handlePassword = (e) => {
-        setPassword(e.target.value)
-    }
+  const handleSubmit = (e) => {
+    console.log("form submitted");
 
-    const handleSubmit = (e) => {
-        console.log('form submitted')
+    axios
+      .post(
+        "http://localhost:3000/sessions",
+        {
+          user: {
+            email: email,
+            password: password,
+          },
+        },
+        { withCredentails: true }
+      )
+      .then((response) => {
+        // console.log("res from login", response);
+        // console.log("res from login.status", response.status);
+        // console.log("res from login.data", response.data);
 
-        axios.post("http://localhost:3000/sessions", {
-            user: {
-                email: email,
-                password: password
-            }},
-            { withCredentails: true }
-        ).then(response => {
-            console.log('res from login', response)
-            if (response.data.logged_in) {
-                handleSuccessfulAuth(response.data)
-            }
-        })
-        .catch(error => {console.log("login errors", error)}) 
-        e.preventDefault()
-    }
+        if (response.data.logged_in === true) {
+            handleSuccessfulAuth(response.data)
+        }
+      })
+      .catch((error) => {
+        console.log("login errors", error);
+      });
+    e.preventDefault();
+  };
 
-    return (
-        <div>
-             <form onSubmit={handleSubmit}>
-                <input                       
-                    type="email"
-                    name="email"
-                    placeholder='Email'
-                    value={email}
-                    onChange={handleEmail}
-                    required
-                />
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmail}
+          required
+        />
 
-                <input                       
-                    type="password"
-                    name="password"
-                    placeholder='Password'
-                    value={password}
-                    onChange={handlePassword}
-                    required
-                />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePassword}
+          required
+        />
 
-                <button type="submit">
-                    Login
-                </button>
-            </form>
-        </div>
-    );
-}
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
